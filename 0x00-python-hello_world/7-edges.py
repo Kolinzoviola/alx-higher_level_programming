2e424d120350c04e119b5583432add56009afd95
Kolinzoviola <dockolinz@gmail.com>
2023-11-27 23:59:54 +0100

    task 7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word[0:3] # Extract the first 3 letters
-word_last_2 = word[-2:]# Extract the last 2 letters
-middle_word =  word[1:-1]# Extract the middle word without the 1st and last let
+word_first_3 = word[0:3]    # Extract the first 3 letters
+word_last_2 = word[-2:]     # Extract the last 2 letters
+middle_word = word[1:-1]   # Extract the middle word without the 1st and last
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
