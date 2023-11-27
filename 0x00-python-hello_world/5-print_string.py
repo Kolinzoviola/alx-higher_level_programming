238ea44c5108198c530972ae953b6b43a7836832
Kolinzoviola <dockolinz@gmail.com>
2023-11-27 23:55:42 +0100

    task 5

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(3 * f"{str}") # 3 times the string
-print(str[0:9]) # first 9 charaters of the string
+print(3 * f"{str}")     # 3 times the string
+print(str[0:9])     # first 9 charaters of the string
